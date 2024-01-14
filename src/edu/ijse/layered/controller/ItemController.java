a94ed531d906e6e5625b7a5b6c7566c7d41b6bc6
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-14 10:25:31 +0530

    Service Layer

--- src/edu/ijse/layered/controller/ItemController.java
+++ src/edu/ijse/layered/controller/ItemController.java
@@ -5,6 +5,8 @@
 package edu.ijse.layered.controller;
 
 import edu.ijse.layered.dto.ItemDto;
+import edu.ijse.layered.service.ServiceFactory;
+import edu.ijse.layered.service.custom.ItemService;
 
 /**
  *
@@ -12,7 +14,9 @@ import edu.ijse.layered.dto.ItemDto;
  */
 public class ItemController {
     
+    private ItemService itemService = (ItemService) ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.ITEM);
+    
     public String saveItem(ItemDto itemDto) throws Exception{
-        return null;
+        return itemService.saveItem(itemDto);
     }
 }
